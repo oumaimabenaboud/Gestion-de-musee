81a1554fdec3e5111a898f9144fec7bf439586aa
oumaimabenaboud <120368654+oumaimabenaboud@users.noreply.github.com>
2022-12-13 16:31:55 +0100

    /

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestmusee.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestmuseeproj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
