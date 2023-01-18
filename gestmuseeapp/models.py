4f9c81611c03c8aa3e40df46c8d4a52bf08568c7
oumaimabenaboud <120368654+oumaimabenaboud@users.noreply.github.com>
2023-01-18 17:57:23 +0100

    pusshing

--- gestmuseeapp/models.py
+++ gestmuseeapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 # Create your models here.
 user = get_user_model()
@@ -12,3 +14,7 @@ class abonne(models.Model):
 
     def __str__(self):
         return self.user.username
+class Registrationform (UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
